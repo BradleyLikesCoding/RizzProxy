b9321ce8ff7bab62aa0e40aec7080523c37d3f34
BradleyLikesCoding <bradleyrohrer0@gmail.com>
2024-05-31 18:58:56 +0000

    started games page, webscraped crazy games for game urls

---
+++ static/public/global/all.js
@@ -0,0 +1,29 @@
+var lettersForSOS = "sos123";
+var sosIndex = 0;
+
+window.addEventListener("keypress", sosKeyPressListener);
+
+document.getElementById("uv-frame").onload(function() {
+  this.contentWindow.document.addEventListener("keypress", sosKeyPressListener);
+});
+
+function sosKeyPressListener(event) {
+  if (event.key == lettersForSOS[sosIndex]) {
+    sosIndex++;
+    if(sosIndex == lettersForSOS.length) {
+      startSos();
+    }
+  } else {
+    sosIndex = 0;
+  }
+}
+
+function startSos() {
+  // retrieve all cookies
+  var Cookies = document.cookie.split(';');
+  // set past expiry to all cookies
+  for (var i = 0; i < Cookies.length; i++) {
+    document.cookie = Cookies[i] + "=; expires=" + new Date(0).toUTCString();
+  }
+  location.href="https://launchpad.classlink.com/aacps";
+}
