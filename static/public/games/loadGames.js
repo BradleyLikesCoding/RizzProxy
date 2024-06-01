b5fb7b764568bde72a0eb44cb4336bb23ba3cb09
Bradley <bradleyrohrer0@gmail.com>
2024-06-01 09:19:03 -0400

    games display but not in line, needs to be fixed, games still cant open

--- static/public/games/loadGames.js
+++ static/public/games/loadGames.js
@@ -1,22 +1,35 @@
-function addGame(game) {
-  document.getElementById("games-container").innerHTML += `
-  <div class="game" onclick="openGame('${game.url}')">
+var gamesJson = [];
+var gamesDiv = document.getElementById("games-container")
+
+
+function getGameHTML(game) {
+  return(`
+  <div class="game" onclick="openGame('${game["url"]}')">
     <div class="game-image-container">
-      <img class="game-image" src="https://images.crazygames.com/${game.image}">
+      <img class="game-image" src="https://images.crazygames.com/${game["image"]}">
     </div>
     <p class="game-title">
-      ${game.name}
+      ${game["name"]}
     </p>
   </div>
-  `;
+  `);
 }
-async function loadGames() {
-  res = await fetch("games.json");
-  json = await res.json();
-  document.getElementById("games-container").innerHTML = "";
-  for (var i = 0; i < await json.length; i++) {
-    addGame(json[i]);
+
+function setGames(page) {
+  var html = '<div class="game-row">';
+  for (var i = page * 200; i < gamesJson.length - (page * 200); i++) {
+    html += getGameHTML(gamesJson[i]);
+    if (((1 + i) - (page * 200)) % 5 == 0) html += '</div><div class="game-row">';
+    if (i - (page * 200) == 199) break;
   }
+  gamesDiv.innerHTML = html + '</div>';
+}
+
+function loadGames() {
+    fetch("games.json").then((res) => res.json()).then((res) => {
+    gamesJson = res;
+    setGames(0)
+  });
 }
 
 loadGames();
