f9a5b07f1f3c488f017608104e29551a1765d3ac
Bradley <bradleyrohrer0@gmail.com>
2024-06-01 15:16:31 -0400

    games working

--- static/public/games/openGame.js
+++ static/public/games/openGame.js
@@ -1,35 +1,19 @@
 "use strict";
-/**
- * @type {HTMLFormElement}
- */
-const form = document.getElementById("uv-form");
-/**
- * @type {HTMLInputElement}
- */
-const address = document.getElementById("uv-address");
-/**
- * @type {HTMLInputElement}
- */
-const searchEngine = document.getElementById("uv-search-engine");
 
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
+function showProxy() {
+  let div = document.getElementById("proxy-div");
+  div.classList = ["show-proxy-div"];
+}
 
+async function openGame(url) {
   try {
     await registerSW();
   } catch (err) {
     alert("Error. Please contact a server administrator.")
   }
-
-  const url = search(address.value, searchEngine.value);
-
-  let frame = document.getElementById("uv-frame");
-  let div = document.getElementById("proxy-div")
   
   showProxy();
 
+  let frame = document.getElementById("uv-frame");
   frame.src = __uv$config.prefix + __uv$config.encodeUrl(url);
-  document.getElementById("nav-bar-address").value = "";
-  address.value = "";
-  document.getElementById("https-lock").innerText = "pending";
-});
+}
