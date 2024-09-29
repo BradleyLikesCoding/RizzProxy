c225adff9993271d142881dec128c1226268df73
bradley <bradleysemail2@gmail.com>
2024-09-29 19:06:30 -0400

    fixed game pages and added lazy loading

--- static/public/games/loadGames.js
+++ static/public/games/loadGames.js
@@ -1,3 +1,5 @@
+var lazyLoad = new LazyLoad();
+
 var gamesJson = [];
 var searchedGamesJson = [];
 var currentPage = 0;
@@ -10,7 +12,7 @@ function getGameHTML(game) {
   return (`
   <div class="game" onclick="openGame('${game["url"]}')">
     <div class="game-image-container">
-      <img class="game-image" src="/image/${game["image"]}">
+      <img class="game-image lazy" src="/image/${game["image"]}">
     </div>
     <p class="game-title">
       ${game["name"]}
@@ -27,6 +29,8 @@ function setGames(page) {
     if (i - (page * pageSize) == pageSize - 1) break;
   }
   gamesDiv.innerHTML = html + '</div>';
+  if(!gamesDiv.childNodes[gamesDiv.childNodes.length - 1].hasChildNodes()) gamesDiv.childNodes[gamesDiv.childNodes.length - 1].remove();
+  lazyLoad.update();
 }
 
 function loadGames() {
@@ -55,14 +59,20 @@ function setPageArrows() {
   var maxPage = getPageCount() - 1;
   if (currentPage < 0) currentPage = 0;
   if (currentPage > maxPage) currentPage = maxPage;
-  document.getElementById("page-number").innerText = currentPage + 1;
+  var pageNumbers = document.getElementsByClassName("page-number");
+  pageNumbers[0].innerHTML = currentPage + 1;
+  pageNumbers[1].innerHTML = currentPage + 1;
   var btns = document.getElementsByClassName("switch-page");
-  if (currentPage == 0) { btns[0].style.cursor = "default" } else { btns[0].style.cursor = "pointer" };
-  if (currentPage == maxPage) { btns[1].style.cursor = "default" } else { btns[1].style.cursor = "pointer" };
+  if (currentPage == 0) { btns[0].style.visibility = "hidden"; btns[2].style.visibility = "hidden" } else { btns[0].style.visibility = "visible"; btns[2].style.visibility = "visible" };
+  if (currentPage == maxPage) { btns[1].style.visibility = "hidden"; btns[3].style.visibility = "hidden" } else { btns[1].style.visibility = "visible"; btns[3].style.visibility = "visible" };
 }
 
 function getPageCount() {
   return Math.ceil(searchedGamesJson.length / pageSize);
+}
+
+function scrollToTopOfGames() {
+  document.getElementsByClassName("main-div")[0].scroll({top: 0,left: 0,behavior: "instant"});
 }
 
 loadGames();
